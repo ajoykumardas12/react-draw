4c8f5c6ed1bf32a5ea9362d4e350a5e91c3ca55e
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-04-20 11:55:12 +0530

    draw-on-mouseMove

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,7 +2,14 @@ import { useOnDraw } from "./Hooks";
 
 const Canvas = ({width, height}) => {
 
-    const setCanvasRef = useOnDraw();
+    const setCanvasRef = useOnDraw(onDraw);
+
+    function onDraw(ctx, point){
+        ctx.fillStyle = "#330066";
+        ctx.beginPath();
+        ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 
     return(
         <canvas 
